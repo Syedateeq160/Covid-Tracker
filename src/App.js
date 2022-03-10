d22af42f7ded71210654c392cf5a62903e01db96
Syed Ateeq <cs172011@dsu.edu.pk>
2022-03-11 00:02:41 +0500

    Data Fetched Task Done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './Components/NavBar';
+import InfoPanel from './Components/InfoPanel'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <InfoPanel />
+    </div>
+  );
+}
+
+export default App;
